d775f218991672dff3dbecb233561d97c96754f4
Putinabillaa <aidiraputiabil@gmail.com>
2023-05-22 13:17:12 +0700

    fix pruning

--- BatchedMatchingBnB.java
+++ BatchedMatchingBnB.java
@@ -4,16 +4,14 @@ public class BatchedMatchingBnB {
     private int N;
     private int[][] costMatrix;
     private boolean[] assigned;
-    private int bestKnownSolution;
 
     BatchedMatchingBnB(int N, int[][] costMatrix){
         this.N = N;
         this.costMatrix = costMatrix;
         this.assigned = new boolean[N];
-        this.bestKnownSolution = Integer.MAX_VALUE;
     }
 
-    private int calculateCost(int riderIdx) {
+    public int calculateCost(int riderIdx) {
         int cost = 0;
         for (int i = riderIdx + 1; i < N; i++) {
             int min = Integer.MAX_VALUE;
@@ -27,7 +25,7 @@ public class BatchedMatchingBnB {
         return cost;
     }
 
-    private void printAssignments(Node min) {
+    public void printAssignments(Node min) {
         if (min.getParent() == null) {
             return;
         }
@@ -39,28 +37,23 @@ public class BatchedMatchingBnB {
         PriorityQueue<Node> pq = new PriorityQueue<>(new NodeComparator());
         Node root = new Node(-1, -1, this.assigned, null, 0, 0);
         pq.add(root);
-        
         while (!pq.isEmpty()) {
             Node min = pq.poll();
             int i = min.getRiderID() + 1;
             if (i == N) {
-                if (min.getCost() < bestKnownSolution) {
-                    bestKnownSolution = min.getCost();
-                }
                 printAssignments(min);
-                return bestKnownSolution;
+                return min.getCost();
             }
             for (int j = 0; j < N; j++) {
                 if (!min.getAssigned()[j]) {
                     int childPathCost = min.getPathCost() + this.costMatrix[i][j];
                     int childCost = childPathCost + calculateCost(i);
-                    if (childCost < bestKnownSolution) {
-                        Node child = new Node(i, j, min.getAssigned(), min, childPathCost, childCost);
-                        pq.add(child);
-                    }
+                    Node child = new Node(i, j, min.getAssigned(), min, childPathCost, childCost);
+                    pq.add(child);
                 }
             }
         }
+
         return -1;
     }
 }
