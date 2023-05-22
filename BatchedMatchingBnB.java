34cf7683fd05a484f517cd21f83b3c9d119b6198
Putinabillaa <aidiraputiabil@gmail.com>
2023-05-22 11:42:49 +0700

    fix: heuristic, pruning

--- BatchedMatchingBnB.java
+++ BatchedMatchingBnB.java
@@ -4,36 +4,30 @@ public class BatchedMatchingBnB {
     private int N;
     private int[][] costMatrix;
     private boolean[] assigned;
+    private int bestKnownSolution;
 
     BatchedMatchingBnB(int N, int[][] costMatrix){
         this.N = N;
         this.costMatrix = costMatrix;
         this.assigned = new boolean[N];
+        this.bestKnownSolution = Integer.MAX_VALUE;
     }
 
-    public int calculateCost(int riderIdx) {
+    private int calculateCost(int riderIdx) {
         int cost = 0;
-        boolean[] available = new boolean[this.N];
-        for (int j = 0; j < N; j++) {
-            available[j] = true;
-        }
         for (int i = riderIdx + 1; i < N; i++) {
             int min = Integer.MAX_VALUE;
-            int minIndex = -1;
-
             for (int j = 0; j < N; j++) {
-                if (!this.assigned[j] && available[j] && costMatrix[i][j] < min) {
-                    minIndex = j;
+                if (!this.assigned[j] && costMatrix[i][j] < min) {
                     min = this.costMatrix[i][j];
                 }
             }
             cost += min;
-            available[minIndex] = false;
         }
         return cost;
     }
 
-    public void printAssignments(Node min) {
+    private void printAssignments(Node min) {
         if (min.getParent() == null) {
             return;
         }
@@ -45,23 +39,28 @@ public class BatchedMatchingBnB {
         PriorityQueue<Node> pq = new PriorityQueue<>(new NodeComparator());
         Node root = new Node(-1, -1, this.assigned, null, 0, 0);
         pq.add(root);
+        
         while (!pq.isEmpty()) {
             Node min = pq.poll();
             int i = min.getRiderID() + 1;
             if (i == N) {
+                if (min.getCost() < bestKnownSolution) {
+                    bestKnownSolution = min.getCost();
+                }
                 printAssignments(min);
-                return min.getCost();
+                return bestKnownSolution;
             }
             for (int j = 0; j < N; j++) {
                 if (!min.getAssigned()[j]) {
                     int childPathCost = min.getPathCost() + this.costMatrix[i][j];
                     int childCost = childPathCost + calculateCost(i);
-                    Node child = new Node(i, j, min.getAssigned(), min, childPathCost, childCost);
-                    pq.add(child);
+                    if (childCost < bestKnownSolution) {
+                        Node child = new Node(i, j, min.getAssigned(), min, childPathCost, childCost);
+                        pq.add(child);
+                    }
                 }
             }
         }
-
         return -1;
     }
 }
